4775c29f6a679a1f800d4e3df67700e24ece452c
Stephen Ball <sball@room77.com>
2015-10-14 11:09:18 -0700

    add missing /public/base/{defs,lite} RULES, fix typo in server_method RULE

---
+++ public/base/lite.h
@@ -0,0 +1,20 @@
+// Copyright 2013 Room77 Inc. All Rights Reserved.
+// Author: pramodg@room77.com (Pramod Gupta)
+
+// Lite common header files.
+// This only includes libs that are needed by almost all libs.
+// These include:
+// 1. defs
+// 2. logging
+// 3. Args.
+
+// WARNING: Please do not add anything else to this file without talking to oztekin@ or pramodg@.
+
+#ifndef _PUBLIC_BASE_LITE_H_
+#define _PUBLIC_BASE_LITE_H_
+
+#include "base/defs.h"
+#include "base/args/args.h"
+#include "base/logging.h"
+
+#endif  // _PUBLIC_BASE_LITE_H_
